0297d4ee967fba1f5fb423c4e36170b5922de083
subraban <136383571+subraban@users.noreply.github.com>
2023-07-09 14:22:31 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "disk_name" {
+  type        = string
+  default     = "disk-2"
+}
+variable "device_name" {
+  type        = string
+  default     = "sqlserver"
+}
+variable "machine" {
+  type        = string
+  default     = "n2-highmem-4"
+}
+variable "name" {
+  type        = string
+  default     = "cloudvm"
+}

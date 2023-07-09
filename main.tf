da72f95f30fa1929ba60a428d8e51c24d9265a40
subraban <136383571+subraban@users.noreply.github.com>
2023-07-09 14:49:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "google_compute_instance" "cloudvm" {
   }
 
   service_account {
-    email  = "101674418114-compute@developer.gserviceaccount.com"
+    email  = var.service_account
     scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
